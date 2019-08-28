84ed5474659a287969dda836c5ffc5134133acd3
Almas <atoimbekov@gmail.com>
2019-08-28 18:16:03 +0600

    Corrected mergeSort

--- src/MergeSort.java
+++ src/MergeSort.java
@@ -17,7 +17,7 @@ public class MergeSort {
 ////        array[25400] = -350;
         int[] array = {13, -3, -5, -25, 20, -3, -16, -23, 18, 20, -7, 12, -5, -22, 15, -4, 7};
 //            long timeBegin = System.nanoTime();
-        new MergeSort().mergeSort(array, array.length);
+        new MergeSort().mergeSort(array);
 //            long timeEnd = System.nanoTime();
 //            count[j] = timeEnd - timeBegin;
 //        }
@@ -27,25 +27,26 @@ public class MergeSort {
         System.out.println(Arrays.toString(array));
     }
 
-    public int[] mergeSort(int[] array, int len) {
+    public void mergeSort(int[] array) {
+        int len = array.length;
         if (len < 2) {
-            return array;
+            return;
         }
         int mid = len / 2;
         int[] leftArr = Arrays.copyOfRange(array, 0, mid);
         int[] rightArr = new int[len - mid];
-        System.arraycopy(array, mid, rightArr, 0, array.length - mid);
+        System.arraycopy(array, mid, rightArr, 0, len - mid);
 
-        mergeSort(leftArr, mid);
-        mergeSort(rightArr, len - mid);
+        mergeSort(leftArr);
+        mergeSort(rightArr);
         merge(leftArr, rightArr, array);
-        return array;
     }
 
     public void merge(int[] leftArr, int[] rightArr, int[] resultList) {
         int positionA = 0, positionB = 0, resultIndex = 0;
 
 //        The first way
+
         while (positionA < leftArr.length && positionB < rightArr.length) {
             if (leftArr[positionA] < rightArr[positionB]) {
                 resultList[resultIndex++] = leftArr[positionA++];
@@ -58,24 +59,27 @@ public class MergeSort {
         System.arraycopy(rightArr, positionB, resultList, resultIndex, rightArr.length - positionB);
 
 //        or instead of System.arraycopy
-//        while (positionA < leftArr.length) {
-//            resultList[resultIndex++] = leftArr[positionA++];
-//        }
-//        while (positionB < rightArr.length) {
-//            resultList[resultIndex++] = rightArr[positionB++];
-//        }
-
+    /*
+        while (positionA < leftArr.length) {
+            resultList[resultIndex++] = leftArr[positionA++];
+        }
+        while (positionB < rightArr.length) {
+            resultList[resultIndex++] = rightArr[positionB++];
+        }
+    */
 //        The third way
-//        for (int i = 0; i < resultList.length; i++) {
-//            if (positionA == leftArr.length) {
-//                resultList[i] = rightArr[positionB++];
-//            } else if (positionB == rightArr.length) {
-//                resultList[i] = leftArr[positionA++];
-//            } else if (leftArr[positionA] < rightArr[positionB]) {
-//                resultList[i] = leftArr[positionA++];
-//            } else {
-//                resultList[i] = rightArr[positionB++];
-//            }
-//        }
+    /*
+        for (int i = 0; i < resultList.length; i++) {
+            if (positionA == leftArr.length) {
+                resultList[i] = rightArr[positionB++];
+            } else if (positionB == rightArr.length) {
+                resultList[i] = leftArr[positionA++];
+            } else if (leftArr[positionA] < rightArr[positionB]) {
+                resultList[i] = leftArr[positionA++];
+            } else {
+                resultList[i] = rightArr[positionB++];
+            }
+        }
+    */
     }
 }
