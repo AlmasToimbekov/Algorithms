cf3ecea7852b5ef7e72ff2778012cc9d35dd57b0
Almas <atoimbekov@gmail.com>
2019-09-18 14:21:21 +0600

    Added random quick sort

--- src/sort/QuickSort.java
+++ src/sort/QuickSort.java
@@ -4,9 +4,15 @@ public class QuickSort {
     public static void quickSort(int[] array, int left, int right) {
         if (left >= right)
             return;
+        int q = partition(array, left, right);
+        quickSort(array, left, q);
+        quickSort(array, q + 1, right);
+    }
+
+    public static int partition(int[] array, int left, int right) {
         int k = array[right - 1];
         int p = left - 1;
-        for (int i = p + 1; i < right; i++) {
+        for (int i = left; i < right; i++) {
             if (array[i] < k) {
                 int temp = array[i];
                 array[i] = array[p + 1];
@@ -16,8 +22,30 @@ public class QuickSort {
         }
         array[right - 1] = array[p + 1];
         array[p + 1] = k;
+        return p + 1;
+    }
 
-        quickSort(array, left, p + 1);
-        quickSort(array, p + 2, right);
+    public static void randomizedQuicksort(int[] array, int left, int right) {
+        if (left < right) {
+            int q = randomizedPartition(array, left, right);
+            randomizedQuicksort(array, left, q);
+            randomizedQuicksort(array, q + 1, right);
+        }
+    }
+
+    public static int randomizedPartition(int[] array, int left, int right) {
+        int i = left + (int)(Math.random() * (right - left));
+        int temp = array[right - 1];
+        array[right - 1] = array[i];
+        array[i] = temp;
+        return partition(array, left, right);
+    }
+
+    public static void tailRecursiveQuicksort(int[] array, int left, int right) {
+        while (left < right) {
+            int q = partition(array, left, right);
+            tailRecursiveQuicksort(array, left, right - 1);
+            left = q + 1;
+        }
     }
 }
